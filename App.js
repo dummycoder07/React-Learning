4935c5a7f483d3d1df0822e19d0b87fa734d8f6a
Aman <sharma.aman.sa456@gmail.com>
2023-09-20 12:06:35 +0530

    Day 3- React

--- App.js
+++ App.js
@@ -1,35 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "Hello World from React"
-// );
+const Title = () => {
+    return <h1> Hello World</h1>
+}
+const num = 10000
+const HeadingComponent = () => (
+  <div>
+    <Title />
+    {Title()}
+    <Title></Title>
+    {num}
+    <h1>Hola World</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     React.createElement("h1", {}, "This is h1 Tags")
-//   )
-// );
-
-const parent2 = React.createElement("div", { id: "parent2" }, [
-  React.createElement(
-    "div",~
-    { id: "child1" },
-    React.createElement("h1", {}, "This is h1-chind1 Tags"),
-    React.createElement(
-      "div",
-      { id: "child2" },
-      React.createElement("h1", {}, "This is h1-child2 Tags")
-    )
-  ),
-]);
-
-root.render(parent2);
+root.render(<HeadingComponent />);
--- README.md
+++ README.md
@@ -6,3 +6,15 @@ React Learning
 - Local Server
 - HMR = hot module replacement 
 - file watching algorithm ( written in c++ )
+- Caching - faster build
+- Image optimization
+- Minification
+- Bundling
+- Compress
+- Consitence Hashing
+- HTTPs
+- Code Splitting
+- Differential Bundling
+- Diagonstic
+- Error Handling
+- Tree Shaking
